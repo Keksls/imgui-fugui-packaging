6065ac42ad43263bcc6511080d79462f5f9d82be
Kekstop <k@k.k>
2026-04-16 11:23:59 +0200

    --

--- ci-build.sh
+++ ci-build.sh
@@ -6,10 +6,18 @@ scriptPath="$(cd "$(dirname "$0")" && pwd)"
 RTYPE="${1:-Release}"
 shift || true
 
-EXTRA_ARGS="$@"
+EXTRA_ARGS=("$@")
+IS_IOS=false
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  $scriptPath/build-native.sh ${RTYPE} -osx-architectures 'arm64;x86_64' ${EXTRA_ARGS}
+for arg in "${EXTRA_ARGS[@]}"; do
+  if [[ "$arg" == "-ios" ]]; then
+    IS_IOS=true
+    break
+  fi
+done
+
+if [[ "$OSTYPE" == "darwin"* && "$IS_IOS" == "false" ]]; then
+  "$scriptPath/build-native.sh" "${RTYPE}" -osx-architectures 'arm64;x86_64' "${EXTRA_ARGS[@]}"
 else
-  $scriptPath/build-native.sh ${RTYPE} ${EXTRA_ARGS}
+  "$scriptPath/build-native.sh" "${RTYPE}" "${EXTRA_ARGS[@]}"
 fi
