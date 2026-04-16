4f59c51a75de6fc3bc05f4342ed53ebd12196b02
Kekstop <k@k.k>
2026-04-16 11:01:09 +0200

    add native ios support

--- build-native.sh
+++ build-native.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env bash
 
+set -e
+
 scriptPath="`dirname \"$0\"`"
 cimguiPath=$scriptPath/cimgui
 
 _CMakeBuildType=Debug
 _CMakeOsxArchitectures=
+_CMakeSystemName=
+_CMakeOsxSysroot=
+_CMakeDeploymentTarget=
+_BuildSubdir=
+__UnprocessedBuildArgs=
 
 while :; do
     if [ $# -le 0 ]; then
         break
     fi
 
-    lowerI="$(echo $1 | awk '{print tolower($0)}')"
+    lowerI="$(echo "$1" | awk '{print tolower($0)}')"
     case $lowerI in
         debug|-debug)
             _CMakeBuildType=Debug
@@ -23,15 +30,68 @@ while :; do
             _CMakeOsxArchitectures=$2
             shift
             ;;
+        -ios)
+            _CMakeSystemName=iOS
+            ;;
+        -ios-platform)
+            _CMakeOsxSysroot=$2
+            shift
+            ;;
+        -ios-deployment-target)
+            _CMakeDeploymentTarget=$2
+            shift
+            ;;
+        -build-subdir)
+            _BuildSubdir=$2
+            shift
+            ;;
         *)
             __UnprocessedBuildArgs="$__UnprocessedBuildArgs $1"
+            ;;
     esac
 
     shift
 done
 
-mkdir -p $cimguiPath/build/$_CMakeBuildType
-pushd $cimguiPath/build/$_CMakeBuildType
-cmake ../.. -DCMAKE_OSX_ARCHITECTURES="$_CMakeOsxArchitectures" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.13 -DCMAKE_BUILD_TYPE=$_CMakeBuildType
+if [ -n "$_BuildSubdir" ]; then
+    buildDir="$cimguiPath/build/$_BuildSubdir/$_CMakeBuildType"
+else
+    buildDir="$cimguiPath/build/$_CMakeBuildType"
+fi
+
+mkdir -p "$buildDir"
+pushd "$buildDir"
+
+CMAKE_ARGS=("../..")
+CMAKE_ARGS+=("-DCMAKE_BUILD_TYPE=$_CMakeBuildType")
+
+if [ -n "$_CMakeOsxArchitectures" ]; then
+    CMAKE_ARGS+=("-DCMAKE_OSX_ARCHITECTURES=$_CMakeOsxArchitectures")
+fi
+
+if [ -n "$_CMakeSystemName" ]; then
+    CMAKE_ARGS+=("-DCMAKE_SYSTEM_NAME=$_CMakeSystemName")
+fi
+
+if [ -n "$_CMakeOsxSysroot" ]; then
+    CMAKE_ARGS+=("-DCMAKE_OSX_SYSROOT=$_CMakeOsxSysroot")
+fi
+
+if [ -n "$_CMakeDeploymentTarget" ]; then
+    CMAKE_ARGS+=("-DCMAKE_OSX_DEPLOYMENT_TARGET=$_CMakeDeploymentTarget")
+elif [[ "$_CMakeSystemName" == "iOS" ]]; then
+    CMAKE_ARGS+=("-DCMAKE_OSX_DEPLOYMENT_TARGET=13.0")
+else
+    CMAKE_ARGS+=("-DCMAKE_OSX_DEPLOYMENT_TARGET=10.13")
+fi
+
+if [ -n "$__UnprocessedBuildArgs" ]; then
+    # shellcheck disable=SC2206
+    EXTRA_ARGS=( $__UnprocessedBuildArgs )
+    CMAKE_ARGS+=("${EXTRA_ARGS[@]}")
+fi
+
+cmake "${CMAKE_ARGS[@]}"
 make
+
 popd
--- ci-build.sh
+++ ci-build.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
+set -e
+
 scriptPath="`dirname \"$0\"`"
-RTYPE="${1}"
+RTYPE="${1:-Release}"
+shift || true
+
+EXTRA_ARGS="$@"
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  $scriptPath/build-native.sh ${RTYPE} -osx-architectures 'arm64;x86_64'
+  $scriptPath/build-native.sh ${RTYPE} -osx-architectures 'arm64;x86_64' ${EXTRA_ARGS}
 else
-  $scriptPath/build-native.sh ${RTYPE}
+  $scriptPath/build-native.sh ${RTYPE} ${EXTRA_ARGS}
 fi
